932c5f37a16434cc508b63f0bf3d65e334b9fc0b
KrijnSlot <krijnslot112233@gmail.com>
2023-12-12 10:39:34 +0100

    Putting first files in

---
+++ GXPEngine2023c/GXPEngine/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GXPEngine;
+
+public class Player : Sprite
+{
+    public Player() : base("circle.png")
+    {
+        x = 100;
+        y = 100;
+    }
+
+    void Update()
+    {
+
+    }
+}
+}
