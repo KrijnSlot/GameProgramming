681998b182463c6379c2c514de82e153a3e26d75
KrijnSlot <krijnslot112233@gmail.com>
2023-12-19 15:18:18 +0100

    player movement

--- GXPEngine2023c/GXPEngine/Player.cs
+++ GXPEngine2023c/GXPEngine/Player.cs
@@ -8,15 +8,17 @@ using GXPEngine;
 
 public class Player : Sprite
 {
+    int playerSpeed = 2;
     public Player() : base("circle.png")
     {
-        x = 100;
-        y = 100;
+        SetOrigin(width / 2, height / 2);
     }
 
     void Update()
     {
-
+        x = (x * 0.96f) + (Input.mouseX * 0.04f);
+        y = (y * 0.96f) + (Input.mouseY * 0.04f);
     }
 }
-}
+
+
